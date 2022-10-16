2f2249461667d17e7ef2f82ee508506dd2c33bfc
Кирилл Овсяников <115923568+Loganlini@users.noreply.github.com>
2022-10-16 11:53:05 +0000

    again change  class Student

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
+# a = 4
+# print('a')
+# print(a)
+# if a == 5:
+# 	print('if 1')
+# elif a > 3:
+# 	print('if 2')
+# elif a < 6:
+# 	print('if 3')
+# a = 0
+# while a<5:
+# 	print('hi')
+# 	a = a + 1
+
+# for i in range(5):
+# 	print(i)
+# print('Hello world')
+# print('This is my first commit')
+
+# class Human:
+# 	def __init__(self):
+# 		self.name = 'None'
+# 		self.age = 0
+# 		self.gender = 'None'
+# 	def introduce(self):
+# 		print('Hello, my name is', self.name)
+# 	def add_info(self):
+# 		self.name = input('Name: ')
+# 		self.age = int(input('Age: '))
+# 		self.gender = input('Gender: ')
+
+# obj = Human('')
+# obj.introduce()
+# obj.add_info()
+# obj.introduce()
+
+from random import *
+
 class Student:
 	def __init__(self, name):
 		self.name = name
@@ -52,4 +90,4 @@ for day in range(365):
 	if obj.alive == False:
 		break
 	obj.live(day)
-			
+		
